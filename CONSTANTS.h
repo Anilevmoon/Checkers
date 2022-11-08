a7a30787f97e6cfedbe1c1edcf27df4abf407b84
Anilevmoon <Anilevmoon@users.noreply.github.com>
2022-11-08 02:33:35 +0200

    upd

--- CONSTANTS.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-namespace constants {
-	static constexpr int s_iWidth = 8;
-	static constexpr int s_iHeight = 8;
-}
--- GameM.cpp
+++
@@ -1,7 +0,0 @@
-#include "GameM.h"
-#include "CONSTANTS.h"
-Game::Game() {}
-
-void Game::PlayTest() {
-	board.BoardDraw(constants::s_iHeight, constants::s_iWidth);
-}
